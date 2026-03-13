06e473c1add066e9ab486ab69179639ae33af1cb
Ananya Rajak <ananyarajak374@gmail.com>
2026-03-13 21:37:45 +0530

    Add files via upload

---
+++ merge_twoSorted_array.java
@@ -0,0 +1,9 @@
+public class merge_twoSorted_array {
+    public static void main(String[] args) {
+        int arr1[]={1,3,5,7};
+        int arr2[]={3,8,9,10};
+        int sorted[]= new int [arr1.length + arr2.length];
+        for(int i=0;i)
+    }
+    
+}
