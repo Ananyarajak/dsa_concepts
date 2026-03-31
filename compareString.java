253fe47126d8c96bf0f2835b4316bfd4065ac60a
Ananya Rajak <ananyarajak374@gmail.com>
2026-03-31 23:22:25 +0530

    Add files via upload

---
+++ compareString.java
@@ -0,0 +1,13 @@
+public class compareString {
+    public static void main(String[] args) {
+        String str1 = "rinkle";
+        String str2 = "twinle";
+
+        if (str1.compareTo(str2) == 0) {
+            System.out.println("strings are equal.");
+        } else {
+            System.out.println("strings are not equal.");
+        }
+    }
+
+}
