b45519faf9461a916a37ca55bb606d3c967eb018
Ananya Rajak <ananyarajak374@gmail.com>
2026-07-13 14:00:48 +0530

    Add files via upload

---
+++ uniqueElement.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int findUnique(vector<int> arr)
+{
+    int ans = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cout << "enter the size of the array "<< endl;
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "enter the element " << endl;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+    int unique = findUnique(arr);
+    cout << "unique element is " << unique << endl;
+}
