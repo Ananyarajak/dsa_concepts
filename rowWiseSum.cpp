d3e4bcfaaeae0a930907120455cb6721f3d5bfe4
Ananya Rajak <ananyarajak374@gmail.com>
2026-07-16 17:42:18 +0530

    Add files via upload

---
+++ rowWiseSum.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void colSum(int arr[][3], int row, int col)
+{
+    cout << "printing row- wise sum " << endl;
+    for (int i = 0; i < row; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < col; j++)
+        {
+            sum = sum + arr[i][j];
+        }
+        cout << sum << endl;
+    }
+}
+int main()
+{
+    int arr[3][3];
+    int row = 3;
+    int col = 3;
+
+    // input
+    cout << "enter the element " << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+        cout << endl;
+    }
+
+    // printing
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
+
+    colSum(arr, row, col);
+}
